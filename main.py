2b82229635111f3ea7562905186752e8f2baba13
Mahmoud Hosseini <hosseini72@gmail.com>
2023-07-25 21:12:18 +0100

    change config name

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from model.models import *
 from model.dtsets import Iris
 import os
 from random import choice
+import pandas as pd
+import numpy as np
 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-
+from sklearn.metrics import log_loss
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import hinge_loss
 
 '''
 This package can be helpful for model selection
@@ -90,11 +94,7 @@ class Train:
 
 
 
-'''
-from mlxtend.plotting import plot_decision_regions
-plot_decision_regions(xtr, ytr, clf= ir, legend =2 )
 
-'''
 
 class Evaluate:
     def __init__(self, dir) -> None:
@@ -108,15 +108,61 @@ class Evaluate:
     def __trained_models(self, model):
         self.__trained_model_dir= os.path.join(self.__path, model)
         self.__trained_configs_list=  os.listdir(self.__trained_model_dir)
-        print(self.__trained_model_dir)
-        print(self.__trained_configs_list)
+        return self.__trained_configs_list
         
     def test(self):
-        pass
+        result_list= []
+        while True:
+            try:
+                mdl= self.__model_directory 
+                mdl_dir= os.path.join(self.__path, mdl)
+                mdl_configs_list= self.__trained_models(mdl)
+                for mdl_config in mdl_configs_list:
+                    with open(f'{mdl_dir}\{str(mdl_config)}', 'br') as file:
+                        trained_mdl= pickle.load(file= file)
+                    result= self.__evaluate(model_name=mdl, config_name=mdl_config, model= trained_mdl)
+                    result_list.append(result)
+                # else:
+                #     print('mdl changed from ', mdl)
+            except StopIteration:
+                break
+        evaluation_df= pd.DataFrame(result_list)
+        evaluation_df.to_csv(f'{self.__path}\MLM.csv')
+        return evaluation_df
 
+
+    
+
+
+
+    def __evaluate(self, model_name, config_name, model):
+        true_labels= None #TODO true labels for test data ytest
+        predicted_labels= model.predict(xtest)
+        predicted_probabilities= model.predict_proba(xtest)
+        predicted_scores= model.score(X, y)
+
+        conf_matrix= confusion_matrix(true_labels, predicted_labels)
+        tn, fp, fn, tp= conf_matrix.ravel()
+        result= {'model': model_name,
+                'config': config_name,
+                'train score': model.score(X, y),
+                'predict score': predicted_scores,
+                'Accuracy': (tp+tn)/(tp+tn+fp+fn),
+                'Precesion': tp/(tp+fp),
+                'Recall': tp/(tp+fn),
+                'Specificity': tn/(tn+fp),
+                'F1 score': 2*tp/(2*tp+fp+fn),
+                'Negative predictive value': tn / (tn + fn),
+                'False positive rate': fp/(fp + tn),
+                'False negative rate': fn/(fn + tp),
+                'mean abolute error': mean_absolute_error(true_labels, predicted_labels),
+                'Log loss': log_loss(true_labels, predicted_probabilities),
+                'Hinge loss': hinge_loss(true_labels, predicted_scores)}
+        return result
+    
 
     def score(self):
-        model_configs= self.__trained_models(mdl)
+        # model_configs= self.__trained_models(mdl)
 
         for _ in range(5):
             mdl= self.__model_directory
@@ -128,27 +174,65 @@ class Evaluate:
     def confusion(self):
         pass
 
-tr= Train()
-tr.fit()
+# tr= Train()
+# tr.fit()
 # print(tr.path)
 # path= 'N:\MLM\trained_models'
-# te= Evaluate(r'N:\MLM\trained_models')
-# te.score()
+te= Evaluate(r'N:\MLM\trained_models_2')
+print(te.test())
 
 '''
-Accuracy
-Confusion matrix
-F1 score
-False positive rate
-False negative rate
-Log loss
-Negative predictive value
-Precession
-Recall
-ROC Curve
-Specificity
-mean abolute error
+conf_matrix= confusion_matrix(true_labels, predicted_labels)
+tn, fp, fn, tp= conf_matrix.ravel()
+Accuracy= (tp+tn)/(tp+tn+fp+fn)
+Precesion= tp/(tp+fp) 
+Recall= tp/(tp+fn)
+Specificity= tn/(tn+fp)
+F1_score= 2*tp/(2*tp+fp+fn)
+Negative_predictive_value= tn / (tn + fn)
+False_positive_rate= fp/(fp + tn)
+False_negative_rate= fn/(fn + tp)
+logloss = log_loss(true_labels, predicted_probabilities)
+mean_abolute_error= mean_absolute_error(true_labels, predicted_labels)
+hinge_loss_value = hinge_loss(true_labels, predicted_scores)
+
+
+from sklearn.metrics import roc_curve
+
+fpr, tpr, thresholds = roc_curve(true_labels, predicted_probabilities)
+
+'''
+cols= ['model', 'config', 'score','Accuracy', 'Precesion', 'Recall', 'Specificity', 'F1 score', 'Negative predictive value',
+       'False positive rate', 'False negative rate', 'mean abolute error', 'Log loss',  'Hinge loss'  ]
+
+
+
+'''
+#Accuracy
+#Confusion matrix
+#F1 score
+#False positive rate
+#False negative rate
+#Log loss
+#Negative predictive value
+##Precesion
+#Recall
+#ROC Curve
+#Specificity
+#mean abolute error
 quantile loss 
 KL Divergence
-Hinge loss 
+#Hinge loss 
+'''
+
+'''
+from mlxtend.plotting import plot_decision_regions
+plot_decision_regions(xtr, ytr, clf= ir, legend =2 )
+
+
+trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.3, random_state=1)
+# predict 
+P = model.predict_proba(testX)
+clf.predict(xtest)
+clf.score(X, y)
 '''
